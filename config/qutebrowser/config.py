6b2757c080491d985a2c033d7a78e64629027967
dummys <dummys1337@gmail.com>
2017-11-29 21:07:44 +0100

    added qutebrowser base config

---
+++ config/qutebrowser/config.py
@@ -0,0 +1,24 @@
+# Uncomment this to still load settings configured via autoconfig.yml
+# config.load_autoconfig()
+
+# Always restore open sites when qutebrowser is reopened.
+c.auto_save.session = True
+
+# Page to open if :open -t/-b/-w is used without URL. Use `about:blank`
+# for a blank page.
+# Type: FuzzyUrl
+c.url.default_page = 'www.google.ch'
+
+c.url.searchengines = {'DEFAULT': 'https://www.google.ch/search?hl=fr&q={}'}
+
+# Page(s) to open at the start.
+# Type: List of FuzzyUrl, or FuzzyUrl
+c.url.start_pages = 'https://www.google.ch'
+
+# Bindings for normal mode
+config.bind('<ctrl+left>', 'tab-prev')
+config.bind('<ctrl+leftarrow>', 'tab-prev')
+config.bind('<ctrl+right>', 'tab-next')
+config.bind('Backspace', 'H')
+config.bind('H', 'back')
+config.bind('backspace', 'back')
