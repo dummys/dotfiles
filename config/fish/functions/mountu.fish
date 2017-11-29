fe18ee19d47c9a7c967be8c9b1dadedb393b830f
dummys <dummys1337@gmail.com>
2017-11-29 13:58:08 +0100

    modified dotfiles that need to go in local repo

--- config/fish/functions/mountu.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function mountu --description 'alias mountu sudo mount -o gid=dummys,fmask=113,dmask=002 '
-	sudo mount -o gid=dummys,fmask=113,dmask=002  $argv;
-end
--- gdbinit
+++
@@ -1,2 +0,0 @@
-source /usr/share/gef/gef.py
-source /home/dummys/works/codz/python/gdb/dps_like_for_gdb.py
