b9bec7dbe5816a4893991d389489cb41612a8f58
dummys <dummys1337@gmail.com>
2017-12-15 11:02:20 +0100

    added ida_ipython script

--- idapro/plugins/ipyida/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf8 -*-
-#
-# This module allows an IPython to be embeded inside IDA.
-#
-# Copyright (c) 2015 Marc-Etienne M.Léveillé <leveille@eset.com>
-# See LICENSE file for redistribution.
